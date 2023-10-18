826e3801b2e96d506289f4c484820fa4d9a55fd6
Guilherme Feier Huff <ghuffg00@gmail.com>
2023-10-18 14:05:55 -0300

    initial header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { AiOutlineWhatsApp } from 'react-icons/ai';
+
+export function Header () {
+  return (
+    <header className="w-full h-14 bg-header-background text-header-foreground">
+      <div className="container m-auto flex justify-start items-center gap-5">
+        <h1 className="font-light text-xl">bazarG</h1>
+        <nav className="font-medium">
+          <ul className="flex h-14 items-center justify-start gap-2">
+            <li>
+              <Link href={"/"} className="px-3 py-2 hover:text-emphasis-background transition	rounded-xl">Home</Link>
+            </li>
+            <li>
+              <Link href={"/info"} className="px-3 py-2 hover:text-emphasis-background transition	rounded-xl">Info</Link>
+            </li>
+          </ul>
+        </nav>
+        <Link href="/contact" className="ml-auto hover:text-green-500 transition">
+          <AiOutlineWhatsApp size={25} />
+        </Link>
+      </div>
+      
+    </header>
+  )
+}
